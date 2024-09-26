d6898370837772e7f8e325f8f43d2dd9f67d2381
Ilya Dashevsky <idashevskii@gmail.com>
2024-09-26 13:44:15 +0300

    added transcribe endpoint

--- README.md
+++ README.md
@@ -42,3 +42,4 @@ Run:
 * [whisper](https://github.com/openai/whisper): Whisper speech recognition (License MIT).
 * [dtw-python](https://pypi.org/project/dtw-python): Dynamic Time Warping (License GPL-3.0).
 * [whisper-timestamped](https://github.com/linto-ai/whisper-timestamped): Whisper with word-level timestamps (License AGPL-3.0).
+* [onnxruntime](https://github.com/microsoft/onnxruntime): Inference and training machine-learning accelerator (License MIT).
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import logging
 import os
+from typing import List
 from fastapi import FastAPI
+import whisper_timestamped as whisper
+import torch
+from pydantic import BaseModel
+import base64
+import tempfile
 
 logging.basicConfig(level=os.environ.get("LOG_LEVEL", "WARNING").upper())
 
@@ -8,6 +14,37 @@ logger = logging.getLogger(__name__)
 
 
 app = FastAPI()
+
+
+class Word(BaseModel):
+    text: str
+    start: float
+    end: float
+
+
+class TranscribeInput(BaseModel):
+    language: str
+    audio: str
+
+
+@app.post("/transcribe")
+def do_transcribe(body: TranscribeInput) -> List[Word]:
+    with tempfile.NamedTemporaryFile() as tmp:
+        tmp.write(base64.b64decode(body.audio))
+        audio = whisper.load_audio(tmp.name)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = whisper.load_model("tiny", device=device)
+    result = whisper.transcribe(model, audio, vad=True, language=body.language)
+
+    ret: List[Word] = []
+
+    for segment in result["segments"]:
+        for word in segment["words"]:
+            ret.append(Word(text=word["text"], start=word["start"], end=word["end"]))
+
+    return ret
+
 
 @app.get("/")
 def read_root():
