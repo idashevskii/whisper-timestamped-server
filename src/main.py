9b30c57a48cb6a6da488aeef5f299e37d4f8a408
Ilya Dashevsky <idashevskii@gmail.com>
2024-09-29 07:30:12 +0300

    exposed model to env config

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 import base64
 import tempfile
 
-logging.basicConfig(level=os.environ.get("LOG_LEVEL", "WARNING").upper())
+logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO").upper())
 
 logger = logging.getLogger(__name__)
 
@@ -32,16 +32,22 @@ def do_transcribe(body: TranscribeInput) -> List[Word]:
     with tempfile.NamedTemporaryFile() as tmp:
         try:
             decoded_audio = base64.b64decode(body.audio)
+            logger.info(f"Received audio size={len(decoded_audio)}")
         except:
             raise HTTPException(400, "Failed to decode base64")
         tmp.write(decoded_audio)
         try:
             audio = whisper.load_audio(tmp.name)
+            logger.info("Audio loaded successfully")
         except:
             raise HTTPException(400, "Failed to decode audio")
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = whisper.load_model("tiny", device=device)
+    logger.info(f"Detected device: {device}")
+    model_name = os.environ["WHISPER_MODEL"]
+
+    model = whisper.load_model(model_name, device=device)
+    logger.info(f"Model '{model_name}' loaded successfully")
     result = whisper.transcribe(model, audio, vad=True, language=body.language)
 
     ret: List[Word] = []
